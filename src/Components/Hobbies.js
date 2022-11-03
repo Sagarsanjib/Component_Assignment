7aeef18501ad327551568a491bc685a7d08142de
sagarsanjib <sanjibchandan123@gmail.com>
2022-11-03 12:04:34 +0530

    component-assignment

---
+++ src/Components/Hobbies.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Hobbies = () => {
+    return (
+        <>
+            <h1> Hobbies</h1>
+            <p>1.Table Tennis</p>
+            <p>2.Reading Books</p>
+            <p>3.Biography </p>
+            <p>4.Teaching</p>
+            <hr />
+        </>
+    )
+}
+
+export default Hobbies;
---
+++ src/Components/Skills.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Skills = () => {
+    return(
+        <>
+        <h1> Skills</h1>
+            <ul>
+                <li>Java</li>
+                <li>JavaScript</li>
+                <li>Testing</li>
+                <li>API Designing</li>
+                <li>System Design</li>
+            </ul>
+            <hr />
+        </>
+        )
+}
+
+export default Skills;
